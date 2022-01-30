11ad5b5107d2d78c8db974eafcf231812f3ac247
Alex Veuthey @pi <alex.veuthey@gmail.com>
2022-01-30 12:48:04 +0100

    Add virtualenv setup script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MiniTV
 Minimalist smart TV interface for Raspberry Pi
+
+## Install the virtualenv
+
+This step needs to be done only once per device, otherwise it results in useless pollution in the `~/.bashrc` file.
+
+```bash
+source install_virtualenv.sh
+```
---
+++ install_virtualenv.sh
@@ -0,0 +1,10 @@
+pip install virtualenvwrapper
+WORKON_HOME=~/.virtualenvs
+mkdir $WORKON_HOME
+echo "# Virtualenv setup" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.9" >> ~/.bashrc
+echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_VIRTUALENV=~/.local/bin/virtualenv" >> ~/.bashrc
+echo "source ~/.local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc
+mkvirtualenv minitv
